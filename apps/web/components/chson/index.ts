a6a460d50e60e169a4e2b41082dd3c8c05ddb873
Carles Andres <carlos.andres.c@gmail.com>
2026-02-20 12:47:03 +0000

    feat(web): add Shiki CSS theme support and export new components
    - Add dual-theme CSS variables for Shiki light/dark modes
    - Export InlineCode and CopyButton from chson components index

--- apps/web/components/chson/index.ts
+++ apps/web/components/chson/index.ts
@@ -1,3 +1,5 @@
 export { Header } from './Header';
 export { Footer } from './Footer';
 export { CodeBlock } from './CodeBlock';
+export { InlineCode } from './InlineCode';
+export { CopyButton } from './CopyButton';
